71cd2b456a7ed897cde5233471fa5ee69256c624
andreas riedmann <andreas@riedmann.rocks>
2023-09-15 08:09:34 +0200

    added the files

---
+++ src/Demo.java
@@ -0,0 +1,20 @@
+public class Demo {
+    public static void main(String[] args) {
+        // typisiert
+
+
+        // Ganze Zahl
+
+        int x = 7; // ganze zah
+        double d = 34.3; // Komma
+        boolean isValid = true;
+        String text = "Hallo";
+
+        for (int i = 0; i < 10; i++) {
+            
+        }
+
+        System.out.print("X:" + x + text );
+        System.out.println("X:" + x + text );
+    }
+}
